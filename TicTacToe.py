6e857a32ccdfd8413e23bb9a77a46ecbfd9e9e42
Enzo0721 <38281095+Enzo0721@users.noreply.github.com>
2020-11-16 09:37:10 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,15 +1,19 @@
 #savegame = open("savegame.txt", "a")
 
 def Entry():
-    print("Welcome to Tic Tac Toe.\n" + "---------------" + "Select one option below:\n" + "Begin - start a new game\n" + "Open - restart a prior game\n")
-    NewGame = str(input("Enter option > "))
+    print("Welcome to Tic Tac Toe.\n" + "------------------------\n" + "Select one option below:\n\n" + "Begin - start a new game\n" + "Open - restart a prior game\n")
+    NewGameStr = str(input("Enter option > "))
+    if (NewGameStr == "Begin","begin"):
+        NewGame = 1
+    elif (NewGameStr == "Open", "open"):
+        NewGame = 0
 
-def Save():
+#def Save():
     #open("savegame.txt", "w")
     #savegame.write()#game data
     #savegame.close()
 
-def ReadSave():
+#def ReadSave():
     #with open("savegame.txt", "r") as f:
        # lines_list = f.readlines()
         #for line in lines_list:
